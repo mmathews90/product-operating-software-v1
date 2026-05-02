b6837ca7a5b790b4d99b85d19e85dc344890720f
Max Mathews <mmathews1990@gmail.com>
2026-05-02 06:12:54 -0700

    update page metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Product Operating Software",
+  description: "The operating system for empowered product teams",
 };
 
 const geistSans = Geist({
